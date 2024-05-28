dc6810f1751609481b131718ec55eb354df8b750
napaintga <123561665+napaintga@users.noreply.github.com>
2024-05-28 14:43:54 +0300

    fix moto page

--- src/pages/moto.tsx
+++ src/pages/moto.tsx
@@ -9,7 +9,7 @@ const MotoPage: React.FC = () => {
   const { addMotorcycle } = useContext(MotoContext);
 
   return (
-    <Box sx={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', bgcolor: 'background.default' }} className='cars-container'>
+    <Box sx={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', bgcolor: 'background.default',paddingTop:{ xs: 18, md: 2 } }} >
       <Container sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, justifyContent: 'center', alignItems: 'center',  paddingTop: 3 }}>
         <Box sx={{  display: 'flex',  justifyContent: 'center', alignItems: 'center' ,}}>
           <MotorcycleForm addMotorcycle={addMotorcycle} />
