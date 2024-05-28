ad564f1fc5ad0fa314ca5dbedaa34372677aad91
napaintga <rina.stka@email.com>
2024-05-28 10:29:48 +0300

    Fix ComingSoonPage

--- src/pages/coming_soon.tsx
+++ src/pages/coming_soon.tsx
@@ -1,8 +1,12 @@
 import { Box, Typography, Stack } from '@mui/material';
 import Countdown from 'react-countdown';
+import { useContext } from 'react';
+import { LocaleContext } from '../components/providers/localeProvider';
 
 const ComingSoonPage = () => {
   const completionDate = new Date('2024-06-01T00:00:00');
+  const { translation } = useContext(LocaleContext);
+  const t = translation.comingSoon;
 
   return (
     <Box sx={{
@@ -13,20 +17,18 @@ const ComingSoonPage = () => {
     }}>
         <Stack direction={{ xs: 'column', md: 'row' }} spacing={3} sx={{ my: 3, padding: 3 }}>
             <Box sx={{ flex: 1 }}>
-      <Typography variant="h2" gutterBottom>
-        Coming Soon
-      </Typography>
-      <Typography variant="h5" gutterBottom>
-        Our website is under construction. We'll be here soon with our new awesome site, subscribe to be notified.
-      </Typography>
-      <Box mt={4}>
-        <Countdown date={completionDate} />
-      </Box>
+                <Typography variant="h2" gutterBottom>
+                    {t.title}
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                    {t.description}
+                </Typography>
+                <Box mt={4}>
+                    <Countdown date={completionDate} />
+                </Box>
+            </Box>
+        </Stack>
     </Box>
-    </Stack>
-
-    </Box>
-
   );
 };
 
